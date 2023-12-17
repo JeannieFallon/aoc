c85c62cb6d833984936ff39b045de2ca9d76dfb9
Jeannie Fallon <jeanniefallon@gmail.com>
2023-12-16 19:46:51 -0500

    WIP day 5, 1

--- 2023/05/05_1.py
+++ 2023/05/05_1.py
@@ -15,7 +15,7 @@ class MapVals:
 
 @dataclass
 class BaseMapping:
-    mappings: list[MapVals] = field(default_factory=list)
+    maps: list[MapVals] = field(default_factory=list)
 
 @dataclass
 class SeedSoil(BaseMapping):
@@ -52,9 +52,33 @@ class Almanac:
     categories: list[str] = field(default_factory=list)
 
 
+def get_mapping(val: int, mapping: list) -> int:
+    is_mapped = False
+    for _map in mapping.maps:
+        #print(f'val: {val}, _map: {_map}')
+        if val >= _map.src and val <= _map.src + _map.length:
+            val = _map.dest + abs(_map.src - val)
+            #print(f'new val: {val}')
+            return val
+
+    return val
+
+
 def get_lowest_location(alm: Almanac) -> int:
-    _sum = 0
-    return _sum
+    locations = []
+
+    for seed in alm.seeds:
+        val = seed
+        for mapping in alm.mappings:
+            #print(mapping)
+            val = get_mapping(val, mapping)
+            print(val)
+            #locations.append(get_mapping(val, mapping))
+            locations.append(val)
+
+    print(locations)
+    locations.sort()
+    return locations[0]
 
 
 # FIXME: there's almost certainly a way to do this within the dataclass
@@ -88,9 +112,7 @@ def main():
         len_lines = len(lines)
 
         # Get seeds from first line
-        seeds = lines[0].split(':')[1].split()
-        for seed in seeds:
-            alm.seeds.append(seed)
+        alm.seeds = [int(seed) for seed in lines[0].split(':')[1].split()]
 
         # Start at first mapping on third line
         for i in range(2, len_lines):
@@ -100,28 +122,27 @@ def main():
             if line != '\n':
                 category = line.split()[0]
 
-                #FIXME should be able to skip this check
-                if category in alm.categories:
-                    # Match category to mapping category
-                    for mapping in alm.mappings:
-                        if category == mapping.category:
-                            curr = mapping
+                # Match category to mapping category
+                for mapping in alm.mappings:
+                    if category == mapping.category:
+                        curr = mapping
 
-                            # Skip header line
+                        # Skip header line
+                        i += 1
+                        while i < len_lines and lines[i] != '\n':
+                            vals = [int(val) for val in lines[i].split()]
+                            map_vals = MapVals(vals[0], vals[1], vals[2])
+                            curr.maps.append(map_vals)
                             i += 1
-                            while i < len_lines and lines[i] != '\n':
-                                vals = lines[i].split()
-                                map_vals = MapVals(vals[0], vals[1], vals[2])
-                                curr.mappings.append(map_vals)
-                                i += 1
 
-                            break
+                        break
 
-
-    # FIXME
+    #print(alm)
+    result += get_lowest_location(alm)
     #print(alm)
 
-    result += get_lowest_location(alm)
+    # FIXME
+    print(result)
 
     # Demo
     assert result == 35
